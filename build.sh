cc27fac9fda420289460cadeb0ec23d0990fa5c4
MQjehovah <1421706030@qq.com>
2026-07-15 18:09:37 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t  agent:latest .
 docker container stop agent
 docker container rm agent
-docker container run -it -d --name agent -p 8090:8080 -p 8091:8081 -v $(pwd)/config:/app/config -v $(pwd)/workspace:/app/workspace agent:latest
+docker container run -it -d --name agent -p 8090:8080 -p 8091:8081 -v $(pwd)/config:/app/config -v $(pwd)/workspace:/app/workspace -v $(pwd)/logs:/app/logs agent:latest
